28967dfb590c7db3cb9514688762b0427967d054
Harold Elías <129306594+HamesEl@users.noreply.github.com>
2025-05-15 10:05:32 -0500

    feat:Add bounded backlog,epics

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {production: true,
                             //serverBasePath: "https://my-json-server.typicode.com/estefanojaque/Managewise-reportes"
                             //serverBasePath: "https://managewise-ffbua6fpfmbteaeq.brazilsouth-01.azurewebsites.net/api/v1"
                             //serverBasePath: "http://localhost:3000"
+                            //manejando buen uso de puerto para el correcto funcionamiento
                             serverBasePath: 'http://localhost:8090/api/v1'
                             };
 
